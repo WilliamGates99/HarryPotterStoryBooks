ffbde51ff670b48a169abfbf84a1bbae28de9dda
WilliamGates99 <william.gates.3299@gmail.com>
2019-10-03 03:21:17 +0330

    .

---
+++ app/src/main/java/com/xeniac/harrypotterstory/BooksActivity.java
@@ -0,0 +1,56 @@
+package com.xeniac.harrypotterstory;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.xeniac.harrypotterstory.adapters.BooksAdapter;
+import com.xeniac.harrypotterstory.dataProviders.BooksDataProvider;
+import com.xeniac.harrypotterstory.models.DataItemBooks;
+
+import java.util.List;
+
+public class BooksActivity extends AppCompatActivity {
+
+    private List<DataItemBooks> dataItemBooksList = BooksDataProvider.dataItemBooksList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+        Toolbar toolbar = findViewById(R.id.toolbar_books);
+        setSupportActionBar(toolbar);
+        booksRecyclerViews();
+
+
+        //Todo move it somewhere else
+        ImageView continueIV = findViewById(R.id.iv_books_continue);
+        continueIV.setClipToOutline(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Todo Bookmarks List Intention Action methods
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void booksRecyclerViews() {
+        BooksAdapter booksAdapter = new BooksAdapter(this, dataItemBooksList);
+        RecyclerView booksRV = findViewById(R.id.rv_books);
+        booksRV.setAdapter(booksAdapter);
+    }
+}
