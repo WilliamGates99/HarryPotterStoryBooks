550822f3b1552122387966d4597923c4bc1bd456
WilliamGates99 <william.gates.3299@gmail.com>
2019-10-30 18:05:02 +0330

    Splash Screen Added

---
+++ app/src/main/java/com/xeniac/harrypotterstory/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.xeniac.harrypotterstory;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, BooksActivity.class));
+        finish();
+    }
+}
