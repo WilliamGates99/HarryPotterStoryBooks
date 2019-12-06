d858229e81f2f5b16bbdfffb2ac0fe7efd26835e
WilliamGates99 <william.gates.3299@gmail.com>
2019-12-06 22:11:44 +0330

    UI Improved

--- app/src/main/java/com/xeniac/harrypotterstory/BooksActivity.java
+++ app/src/main/java/com/xeniac/harrypotterstory/BooksActivity.java
@@ -3,6 +3,7 @@ package com.xeniac.harrypotterstory;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -12,10 +13,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.xeniac.harrypotterstory.adapters.BooksAdapter;
@@ -93,21 +94,23 @@ public class BooksActivity extends AppCompatActivity {
         chaptersDataSource = new ChaptersDataSource(this);
         SharedPreferences preferences = getSharedPreferences(READING_CHECK, MODE_PRIVATE);
         int readingChapterId = preferences.getInt(READING_CHECK_KEY, 0);
-        FrameLayout continueFL = findViewById(R.id.fl_books_continue);
+        LinearLayout continueLL = findViewById(R.id.ll_books_continue);
 
         if (readingChapterId == 0) {
-            continueFL.setVisibility(View.GONE);
+            continueLL.setVisibility(View.GONE);
         } else {
             chaptersDataSource.open();
             DataItemChapters item = chaptersDataSource.getReadingItem(readingChapterId);
 
+            CardView continueCV = findViewById(R.id.cv_books_continue);
             ImageView continueIV = findViewById(R.id.iv_books_continue);
-            LinearLayout continueLL = findViewById(R.id.ll_books_continue);
+            RelativeLayout continueRL = findViewById(R.id.rl_books_continue);
             TextView continueChapterTV = findViewById(R.id.tv_books_continue_chapter_title);
             TextView continueBookTV = findViewById(R.id.tv_books_continue_book_title);
             ProgressBar continuePB = findViewById(R.id.pb_books_continue);
 
-            continueFL.setVisibility(View.VISIBLE);
+            continueCV.setClipToOutline(false);
+            continueLL.setVisibility(View.VISIBLE);
             continueChapterTV.setText(item.getTitle());
             continueBookTV.setText(booksDataSource.getBookTitle(item.getBookId()));
             continuePB.setProgress((int) ((float) item.getReadScroll() * 100 / item.getTotalScroll()));
@@ -122,7 +125,7 @@ public class BooksActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            continueLL.setOnClickListener(v -> {
+            continueRL.setOnClickListener(v -> {
                 Intent intent = new Intent(BooksActivity.this, PagesActivity.class);
                 intent.putExtra(ChaptersAdapter.ITEM_KEY, item);
                 startActivity(intent);
