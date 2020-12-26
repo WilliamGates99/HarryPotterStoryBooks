2bc9ca18c09bdc1d5978011a50a38a876410ae65
WilliamGates99 <william.gates.3299@gmail.com>
2020-12-26 22:35:43 +0330

    Add Chapter 3 of Book 2

--- app/src/main/java/com/xeniac/harrypotterstory/dataProviders/PagesDataProviderBook2/PagesDataProviderBook2_3.java
+++ app/src/main/java/com/xeniac/harrypotterstory/dataProviders/PagesDataProviderBook2/PagesDataProviderBook2_3.java
@@ -23,14 +23,6 @@ public class PagesDataProviderBook2_3 {
         addItem(new DataItemPages(238, 8, R.string.string_book_2_chapter_3_page_8, 23));
         addItem(new DataItemPages(239, 9, R.string.string_book_2_chapter_3_page_9, 23));
         addItem(new DataItemPages(2310, 10, R.string.string_book_2_chapter_3_page_10, 23));
-        addItem(new DataItemPages(2311, 11, R.string.string_book_2_chapter_3_page_11, 23));
-        addItem(new DataItemPages(2312, 12, R.string.string_book_2_chapter_3_page_12, 23));
-        addItem(new DataItemPages(2313, 13, R.string.string_book_2_chapter_3_page_13, 23));
-        addItem(new DataItemPages(2314, 14, R.string.string_book_2_chapter_3_page_14, 23));
-        addItem(new DataItemPages(2315, 15, R.string.string_book_2_chapter_3_page_15, 23));
-        addItem(new DataItemPages(2316, 16, R.string.string_book_2_chapter_3_page_16, 23));
-        addItem(new DataItemPages(2317, 17, R.string.string_book_2_chapter_3_page_17, 23));
-        addItem(new DataItemPages(2318, 18, R.string.string_book_2_chapter_3_page_18, 23));
     }
 
     private static void addItem(DataItemPages item) {
