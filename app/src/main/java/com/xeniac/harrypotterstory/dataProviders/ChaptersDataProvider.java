c63e872518df1902e0f954783725fa7647150a06
WilliamGates99 <william.gates.3299@gmail.com>
2020-12-29 20:39:18 +0330

    Add Chapter 2 of Book 3

--- app/src/main/java/com/xeniac/harrypotterstory/dataProviders/ChaptersDataProvider.java
+++ app/src/main/java/com/xeniac/harrypotterstory/dataProviders/ChaptersDataProvider.java
@@ -57,7 +57,7 @@ public class ChaptersDataProvider {
 
         //Book 3
         addItem(new DataItemChapters(31, 1, R.string.string_book_3_chapters_title_1, 15, 0, 0, 3, "ic_cover_chapters_3_1.jpg", false, true));
-        addItem(new DataItemChapters(32, 2, R.string.string_book_3_chapters_title_2, 15, 0, 0, 3, "ic_cover_chapters_3_2.jpg", false, false));
+        addItem(new DataItemChapters(32, 2, R.string.string_book_3_chapters_title_2, 15, 0, 0, 3, "ic_cover_chapters_3_2.jpg", false, true));
         addItem(new DataItemChapters(33, 3, R.string.string_book_3_chapters_title_3, 18, 0, 0, 3, "ic_cover_chapters_3_3.jpg", false, false));
         addItem(new DataItemChapters(34, 4, R.string.string_book_3_chapters_title_4, 20, 0, 0, 3, "ic_cover_chapters_3_4.jpg", false, false));
         addItem(new DataItemChapters(35, 5, R.string.string_book_3_chapters_title_5, 27, 0, 0, 3, "ic_cover_chapters_3_5.jpg", false, false));
